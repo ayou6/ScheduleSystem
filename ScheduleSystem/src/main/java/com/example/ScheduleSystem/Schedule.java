1f99ff5140c5857413c9741b082bc101a703e8fa
ayou6 <ayoxb.abbas@gmail.com>
2023-05-30 16:22:49 +0400

    Last Commit

--- ScheduleSystem/src/main/java/com/example/ScheduleSystem/Schedule.java
+++ ScheduleSystem/src/main/java/com/example/ScheduleSystem/Schedule.java
@@ -1,16 +1,10 @@
 package com.example.ScheduleSystem;
-
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Schedule {
     @Scheduled (cron ="0 */10 9-17 * * 0-4")
-
-
-
     public void ScheduleTest(){
-        System.out.println("Wassup!!");
-
-    }
+        System.out.println("Wassup!!");}
 }
