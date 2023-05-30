8c2455d7c23bd35cf8280e753e575a38cb66e31d
ayou6 <ayoxb.abbas@gmail.com>
2023-05-30 14:03:27 +0400

    problem no5 solved

--- ScheduleSystem/src/main/java/com/example/ScheduleSystem/Schedule.java
+++ ScheduleSystem/src/main/java/com/example/ScheduleSystem/Schedule.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Schedule {
-    @Scheduled (cron ="0 0 8 * *")
+    @Scheduled (cron ="0 0 3 1 * 1")
 
     public void ScheduleTest(){
         System.out.println("Wassup!!");
